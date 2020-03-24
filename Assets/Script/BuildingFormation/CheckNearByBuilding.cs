94770e6f1a6aa61639d543855e137030573244a5
Soemyatmin <17348039+Soemyatmin@users.noreply.github.com>
2020-03-24 08:27:27 +0630

    sth

---
+++ Assets/Script/BuildingFormation/CheckNearByBuilding.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckNearByBuilding : MonoBehaviour {
+
+    void Update() {
+        CheckingBuilding();
+    }
+
+    public bool CheckingBuilding() {
+        Collider[] m_HitDetect = Physics.OverlapBox(transform.position + new Vector3(0, 1.5f, 0), new Vector3(8.2f, 3.2f, 8) /2, Quaternion.identity);
+        if (m_HitDetect.Length > 0) {
+            for (int i = 0; i < m_HitDetect.Length; i++) {
+                if (m_HitDetect[i].tag == "building") {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void BuildAction() {
+        GetComponent<Collider>().enabled = true;
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(transform.position + new Vector3(0, 1.5f, 0), new Vector3(8.2f, 3.2f, 8));
+    }
+}
---
+++ Assets/Script/Camera/CameraMove.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CameraMove : MonoBehaviour {
+
+    public GameObject[] Buttons;
+    public float CameraMovementSpeed;
+
+    ScreenPosition screenPosition = ScreenPosition.None;
+    Vector3 moveDir;
+
+    [System.Serializable]
+    public enum ScreenPosition {
+        Up,
+        Down,
+        Left,
+        Right,
+        None
+    };
+
+    void Start() {
+        moveDir = Vector3.zero;
+        for (int i = 0; i < Buttons.Length; i++) {
+            HoverTrigger(Buttons[i]);
+            HoverTriggerStop(Buttons[i]);
+        }
+    }
+
+    void Update() {
+        switch (screenPosition) {
+            case ScreenPosition.Up:
+                moveDir = transform.forward;
+                break;
+            case ScreenPosition.Down:
+                moveDir = -transform.forward;
+                break;
+            case ScreenPosition.Left:
+                moveDir = -transform.right;
+                break;
+            case ScreenPosition.Right:
+                moveDir = transform.right;
+                break;
+            case ScreenPosition.None:
+                moveDir = Vector3.zero;
+                break;
+        }
+        transform.localPosition += moveDir * CameraMovementSpeed * Time.deltaTime;
+    }
+
+
+    public void HoverTrigger(GameObject fmitem) {
+        EventTrigger pointerHoverTrigger = fmitem.GetComponent<EventTrigger>();
+        EventTrigger.Entry yourNewEntry = new EventTrigger.Entry();
+        yourNewEntry.eventID = EventTriggerType.PointerEnter;
+        pointerHoverTrigger.triggers.Add(yourNewEntry);
+        yourNewEntry.callback.AddListener((eventData) => {
+            CameraViewMove(fmitem.GetComponent<CameraMovingBtn>().ButtonPosition);
+        });
+    }
+
+    public void HoverTriggerStop(GameObject fmitem) {
+        EventTrigger pointerHoverTrigger = fmitem.GetComponent<EventTrigger>();
+        EventTrigger.Entry yourNewEntry = new EventTrigger.Entry();
+        yourNewEntry.eventID = EventTriggerType.PointerExit;
+        pointerHoverTrigger.triggers.Add(yourNewEntry);
+        yourNewEntry.callback.AddListener((eventData) => {
+            CameraViewStop();
+        });
+    }
+
+
+    public void CameraViewMove(ScreenPosition screenPosition) {
+        this.screenPosition = screenPosition;
+    }
+
+    public void CameraViewStop() {
+        screenPosition = ScreenPosition.None;
+    }
+}
